5df7d97a4fa328c438fb02f9cd1f494a517b08b2
Raphael <rj@hodo.fr>
2026-01-15 23:11:48 +0100

    multiverse

--- codebase.sh
+++
@@ -1,6 +0,0 @@
-find . -type d \( -name .git -o -name __pycache__ \) -prune -o \
-     -type f ! -path '*/.git/*' ! -path '*/__pycache__/*' \
-            ! -name '.envi.secret*' ! -name '*.pyc' ! -name '*.pyo' \
-            ! -name '*.txt' ! -name '*codebase.md' ! -name 'tous_*.txt' \
-     -exec sh -c 'printf "\n%s\n────────────────────────────────────────────────────────────\n\n" "{}" && cat "{}" && printf "\n\n"' \; \
-     > codebase.md
